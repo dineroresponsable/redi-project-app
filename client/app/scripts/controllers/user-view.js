46f7cd05f35730ac1a49bd823a6bba1f89ff49a6
Ivan Siachoque <ivanfernandosia@gmail.com>
2016-04-10 14:46:12 -0500

    fix chart sytles

--- client/app/scripts/controllers/user-view.js
+++ client/app/scripts/controllers/user-view.js
@@ -5,29 +5,37 @@ angular.module('rediApp')
   		$scope.viewUser = true;
   		var userObj = User.one($routeParams.id);
   		userObj.get().then(function(data) {
-  			$scope.totalIncomes = 0,
-  			$scope.totalExpenses = 0,
+  			$scope.totalIncomes = 0;
+  			$scope.totalExpenses = 0;
   			$scope.cash = 0;
 
   			$scope.user = data;
-  			// incomes doughnut
+        $scope.chartColours = ['#64A75B', '#EA9049', '#E06865', '#91C27E', '#3887C2', '#0098C0'];
+
+        // total values
+        $scope.totalIncomes = data.incomes.category1.value + data.incomes.category2.value + data.incomes.category3.value;
+        $scope.totalExpenses = data.expenses.category1.value + data.expenses.category2.value + data.expenses.category3.value;
+        $scope.cash = $scope.totalIncomes - $scope.totalExpenses;
+
+        $scope.chartOptions = {
+          multiTooltipTemplate : function (label) {
+            return label.datasetLabel + ': ' +    label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+          },
+          legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%> $<%= segments[i].value %><%}%></li><%}%></ul>"
+        };
+
+  			// incomes pie
   			$scope.incomesLabels = [data.incomes.category1.name, data.incomes.category2.name, data.incomes.category3.name];
   			$scope.incomesData = [data.incomes.category1.value, data.incomes.category2.value, data.incomes.category3.value];
-  			// expenses doughnut
+
+  			// expenses pie
   			$scope.expensesLabels = [data.expenses.category1.name, data.expenses.category2.name, data.expenses.category3.name];
   			$scope.expensesData = [data.expenses.category1.value, data.expenses.category2.value, data.expenses.category3.value];
 
-  			$scope.totalIncomes = data.incomes.category1.value + data.incomes.category2.value + data.incomes.category3.value;
-  			$scope.totalExpenses = data.expenses.category1.value + data.expenses.category2.value + data.expenses.category3.value;
-  			$scope.cash = $scope.totalIncomes - $scope.totalExpenses;
-
-  			// total pie
-  			//$scope.totalLabels = ['Total Ingresos', 'Total Gastos', 'Ahorro'];
-  			//$scope.totalData = [$scope.totalIncomes, $scope.totalExpenses, $scope.cash];
+        // total bars chart
   			$scope.totalTime = ['Hoy'];
-			$scope.totalLabels = ['Ingresos', 'Gastos', 'Balance'];
-
-			$scope.totalData = [
+			  $scope.totalLabels = ['Ingresos', 'Gastos', 'Balance'];
+			  $scope.totalData = [
 			    [100],
 			    [($scope.totalExpenses * 100 / $scope.totalIncomes)],
 			    [($scope.cash * 100 / $scope.totalIncomes)]
