9a249aae6ed6fc8c3efcae12217d7964d5805285
Ivan Siachoque <ivanfernandosia@gmail.com>
2016-04-10 00:10:38 -0500

    add hide and show steps

--- client/app/scripts/controllers/user-add.js
+++ client/app/scripts/controllers/user-add.js
@@ -3,10 +3,24 @@
 angular.module('rediApp')
   .controller('UserAddCtrl', function ($scope, User, $location) {
 		$scope.user = {};
-		$scope.user.totals.totalExpenses = 0;
+		$scope.step2 = false;
+		$scope.step3 = false;
+		$scope.step4 = false;
 		$scope.saveUser = function() {
 			User.post($scope.user).then(function() {
 				$location.path('/users');
 			});
+		};
+		$scope.toStep2 = function() {
+			$scope.step2 = true;
+			console.log($scope.step2);
+		};
+		$scope.toStep3 = function() {
+			$scope.step2 = false;
+			$scope.step3 = true;
+		};
+		$scope.toStep4 = function() {
+			$scope.step3 = false;
+			$scope.step4 = true;
 		};
   });
